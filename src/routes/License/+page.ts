dfd15e68ad781b5c51e2f5d4b7898589a014ff1d
theseeduse <qwert304411@naver.com>
2024-11-23 18:35:33 +0900

    Update +page.ts

--- src/routes/License/+page.ts
+++ src/routes/License/+page.ts
@@ -4,11 +4,12 @@ export const load: PageLoad = async ({ fetch }) => {
 	const response = await fetch('/api/public/v');
 	const data = await response.json();
 
-	const { engine, backend, frontend } = data;
+	const { engine, backend, frontend, skin } = data;
 
 	return {
 		engine_v: engine,
 		back_v: backend,
 		front_v: frontend,
+		skin_v: skin,
 	};
 };
